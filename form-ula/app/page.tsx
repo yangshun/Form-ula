f53bfab31e6f5882944ed0dd5eb2530d85270aa0
Guo Keming <e1398747@u.nus.edu>
2026-01-22 13:34:04 +0800

    Fix NavBar
    Allows Navbar to toggle between view and Homepage
    
    FIx TextField
    
    Todo Link the text Field to the Prview Form

--- form-ula/app/page.tsx
+++ form-ula/app/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 import Card from '@mui/material/Card';
 import { useRouter } from 'next/navigation';
-
-
-// Component
+import { TextField } from '@mui/material';
+import { useState } from 'react';
+// Components
 import { NavBar } from "@/components/navbar";
 import { LeftSidebar } from "@/components/leftSidebar";
 
 
 const Home = () => {
+  const [title, settitle] = useState('');
+  const [description, setDescription] = useState('');
   const router = useRouter(); 
+  
   return (
     <main className="flex min-h-screen flex-col">
       {/* Header NavBar */}
@@ -23,9 +26,30 @@ const Home = () => {
         <div className="p-8">
           <Card>
             {/* Header */}
-            <div className="p-8 items-center bg-purple-100">
-              <h2 className="text-2xl font-bold">Form Title</h2>
-              <p>Form Description.</p>
+            <div className="px-8 items-center bg-purple-100">
+              <TextField
+                fullWidth
+                placeholder="Form Title"
+                variant="standard"
+                value={title}
+                onChange={(e) => settitle(e.target.value)}
+                margin="normal"
+                InputProps={{
+                  disableUnderline: true,
+                  style: { fontSize: '30px' ,fontWeight: 'bold'},
+                }}
+              />
+              <TextField
+                fullWidth
+                placeholder="Form Description"
+                variant="standard"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                margin="normal"
+                InputProps={{
+                  disableUnderline: true,
+                }}
+                />      
             </div>
             {/* Body */}
             <div className="h-64 flex items-center justify-center">
--- form-ula/components/navbar.tsx
+++ form-ula/components/navbar.tsx
@@ -3,10 +3,11 @@
 import Image from "next/image";
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Save';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 export const NavBar = () => {
     const router = useRouter(); 
+    const isPreview = (usePathname() === "/PreviewForm");
     return (
       <div className="flex boder-b shadow-md border-gray-200">
         <div className="max-w-6xl w-full mx-auto flex items-center justify-between py-4 px-8">
@@ -25,8 +26,8 @@ export const NavBar = () => {
             <Button variant="outlined" startIcon={<SendIcon/>} sx={{ color: 'black', borderColor: 'black'}}>
               <h1>Save</h1>
             </Button>
-            <Button variant="outlined" sx={{ color: 'black', borderColor: 'black'}} onClick={() => router.push('/PreviewForm')}>
-              Preview Form
+            <Button variant="outlined" sx={{ color: 'black', borderColor: 'black'}} onClick={() => router.push(isPreview? '/' :'/PreviewForm')}>
+              <h1>{isPreview ? 'Back to Edit' : 'Preview Form'}</h1>
             </Button>
           </div>
         </div>
